820f59d307b9744874b664a62dbc31abc9cb9f94
Denis Fisenko <monahSpb79@yandex.ru>
2021-12-05 07:11:24 +0300

    first_app

---
+++ my-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom  from 'react-dom'
+
+const element = <h1>Hello World, меня зовут: Денис Фисенко!</h1>
+
+ReactDom.render(element, document.getElementById('root'))
